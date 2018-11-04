3e45a4f5c601454dbfeeb0b96764dbd8491a9ffd
Lubos Bosak <lubos.bosak@gmail.com>
2018-11-04 19:41:32 +0100

    removing TODOs

--- src/main/java/com/inspired/restaurant/RestaurantApplication.java
+++ src/main/java/com/inspired/restaurant/RestaurantApplication.java
@@ -3,15 +3,6 @@ package com.inspired.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//refactoring:
-//TODO: getReservationTimeSlots in ReservationmanagerImpl
-
-//features:
-//TODO: optimize table selection for party size
-//TODO: add smoking/non-smoking feature (TERRACE => smoking)
-//TODO: change reservation time slot granularity to 30 minutes
-//TODO: extend reservations also to lunch hours
-
 @SpringBootApplication
 public class RestaurantApplication {
 
